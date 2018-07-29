9df7cc5784db2103a3f4a3a0e79a0298e6475bfa
monjason23 <mfabico@mfabicos-MacBook-Pro.local>
2018-07-29 23:12:22 +0800

    c

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.get("/hello", function(req, res) {
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.resolve(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-  });
+  // app.use(express.static(path.resolve(__dirname, "client/build")));
+  // // Handle React routing, return all requests to React app
+  // app.get("*", function(req, res) {
+  //   res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  // });
 }
 
 app.listen(port, function() {
