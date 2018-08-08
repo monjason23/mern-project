c219c5b472f784bf698d9beb455645da064a01ec
monjason23 <mfabico@mfabicos-MacBook-Pro.local>
2018-08-08 23:14:13 +0800

    modified folder

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var cors = require("cors");
+
+var app = express();
+
+var port = process.env.PORT || 4500;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/hello", function(req, res) {
+  res.status(200).send({ connected: "Now connected" });
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "client/build")));
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(port, function() {
+  console.log("Server running at port " + port);
+});
