53c49c21af4daa3d1f366b9e1ee1282dc37ab633
monjason23 <mfabico@mfabicos-MacBook-Pro.local>
2018-08-08 23:34:02 +0800

    attempt1

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 var app = express();
+var path = require("path");
 
 var port = process.env.PORT || 4500;
 
@@ -12,7 +13,13 @@ app.get("/hello", function(req, res) {
   res.status(200).send({ connected: "Now connected" });
 });
 
-require("./config/config");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "client/build")));
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(port, function() {
   console.log("Server running at port " + port);
