57e747e6b6127eb569b585227969114e60889073
monjason23 <mfabico@mfabicos-MacBook-Pro.local>
2018-08-08 23:19:41 +0800

    Modiy server folder

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 var cors = require("cors");
-
 var app = express();
+
+require("./config/config");
 
 var port = process.env.PORT || 4500;
 
@@ -13,14 +13,6 @@ app.use(cors());
 app.get("/hello", function(req, res) {
   res.status(200).send({ connected: "Now connected" });
 });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "client/build")));
-
-  app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-  });
-}
 
 app.listen(port, function() {
   console.log("Server running at port " + port);
